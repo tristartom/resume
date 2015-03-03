3a55ccabdbd6d8efbcdfb4066fad42291594573f
Yuzhe Tang <tristartom@gmail.com>
2015-03-03 00:50:25 -0500

    first commit by me

---
+++ README.mk
@@ -0,0 +1,9 @@
+Setup
+===
+
+wkhtmltopdf: [[download](http://wkhtmltopdf.org/downloads.html)]
+
+pandoc: [[download-mac](https://github.com/jgm/pandoc/releases/download/1.13.2/pandoc-1.13.2-osx.pkg)], [[tutorial](http://johnmacfarlane.net/pandoc/getting-started.html)]
+
+
+
